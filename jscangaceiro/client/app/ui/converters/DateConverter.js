e81847c3b4176e47db65d4e1f7c0dcef925a202e
Samuel Borges <sammy_rb@hotmail.com>
2022-06-30 21:26:27 -0300

    ajuste campo data e criando captura de erro para entrada invalida da data

--- jscangaceiro/client/app/ui/converters/DateConverter.js
+++ jscangaceiro/client/app/ui/converters/DateConverter.js
@@ -1,7 +1,7 @@
 class DateConverter {
 
     constructor() {
-        throw new Error('Esta classe não pode ser instanciada');
+        throw new DataInvalidaException();
     }
 
     static paraTexto(data) {
@@ -9,11 +9,13 @@ class DateConverter {
     }
 
     static paraData(texto) {
-        if(!/^\d{4}-\d{2}-\d{2}$/.test(texto)) {
-            throw new Error('Data deve estar no formato aaaa-mm-dd');
+        if(!/\d{2}\/\d{2}\/\d{4}/.test(texto)) {
+            throw new Error('Data deve estar no formato dd/mm/aaaa');
         }
 
-        return new Date(...texto.split('-').map((item, indice) => item - indice % 2));
+        return new Date(...texto.split('/')
+            .reverse()
+            .map((item, indice) => item - indice % 2));
         
     }
 }
