ec92fda4ac3223d218dbca9f0b581e6b1df65b7e
Samuel Borges <sammy_rb@hotmail.com>
2022-07-03 19:12:50 -0300

    desenvolvido a importacao de negociacao com XMLHttpRequest - cap 12

--- jscangaceiro/client/app/controllers/NegociacaoController.js
+++ jscangaceiro/client/app/controllers/NegociacaoController.js
@@ -19,6 +19,8 @@ class NegociacaoController {
             'texto'
         );
 
+        this._service = new NegociacaoService();
+
     }
 
     adiciona(event) {
@@ -57,6 +59,21 @@ class NegociacaoController {
     apaga() {
         this._negociacoes.esvazia();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
+    }
+
+    importaNegociacoes() {
+        this._service.obterNegociacoesDaSemana((err, negociacoes) => {
+            
+            if(err) {
+                this._mensagem.texto = 'Não foi possível obter as negociações da semana';
+                return;
+            }
+
+            negociacoes.forEach( negociacao => 
+                this._negociacoes.adiciona(negociacao));
+
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        });
     }
 
 }
