515994293d7d27d83b782b67164aa6fa2ec06555
Samuel Borges <sammy_rb@hotmail.com>
2022-08-01 15:44:27 -0300

    implementado async/await e debounce

--- jscangaceiro/client/app-src/app.js
+++ jscangaceiro/client/app-src/app.js
@@ -1,6 +1,8 @@
 import { NegociacaoController } from './controllers/NegociacaoController.js';
+import { debounce } from './util/index.js';
 
 const controller = new NegociacaoController();
+
 const $ = document.querySelector.bind(document);
 
 $('.form')
@@ -10,4 +12,7 @@ $('#botao-apaga')
     .addEventListener('click', controller.apaga.bind(controller));
 
 $('#botao-importa')
-    .addEventListener('click', controller.importaNegociacoes.bind(controller));
+    .addEventListener('click', debounce(() => {
+        console.log('EXECUTOU A OPERAÇÃO DO DEBOUNCE');
+        controller.importaNegociacoes();
+    }, 1000));
